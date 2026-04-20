d9564fa1df3890c4cd36d21a620af2c5bdc362bd
jeanpaullopes <jean.lopes@ulife.com.br>
2026-04-20 20:11:43 -0300

    v2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="center">
     <h1>You did it!</h1>
-    <PostsCard qtdPosts="0" />
+    <PostsCard :qtdPosts="nPosts" />
     <p>
       Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
       documentation
@@ -14,6 +14,23 @@ export default {
   name: 'App',
   components: {
     PostsCard,
+  },
+  data() {
+    return {
+      nPosts: 0,
+    }
+  },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        console.log(data.length)
+        this.nPosts = data.length
+      })
+      .catch((error) => {
+        console.error('Error fetching posts:', error)
+      })
   },
 }
 

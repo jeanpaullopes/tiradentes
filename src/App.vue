ddda0f313d9ad4488d93f5918cc0a680402c324a
jeanpaullopes <jean.lopes@ulife.com.br>
2026-04-20 20:04:36 -0300

    Tiradentes

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="center">
+    <h1>You did it!</h1>
+    <PostsCard qtdPosts="0" />
+    <p>
+      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+      documentation
+    </p>
+  </div>
+</template>
+<script>
+import PostsCard from './components/PostsCard.vue'
+export default {
+  name: 'App',
+  components: {
+    PostsCard,
+  },
+}
+
+//https://jsonplaceholder.typicode.com/posts
+</script>
+<style scoped>
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
---
+++ src/components/PostsCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="caixa">
+    <h1 v-if="qtdPosts > 0">{{ qtdPosts }} posts</h1>
+    <h1 v-else>Não há posts</h1>
+    <p v-if="qtdPosts > 0">View all posts</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PostsCard',
+  props: {
+    qtdPosts: {
+      type: Number,
+      default: 0,
+    },
+  },
+}
+</script>
+<style scoped>
+.caixa {
+  border: solid 1px black;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 10px 10px 5px #888888;
+}
+</style>
